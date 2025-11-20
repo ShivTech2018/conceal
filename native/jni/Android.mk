e2751ce66e8f5219a2b4b9747d275f5890c13e36
ShivTech <shivtech2018@gmail.com>
2025-11-20 15:51:25 +0530

    update

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -4,14 +4,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := concealjni
 LOCAL_CFLAGS    := -fvisibility=hidden -Os -fdata-sections -ffunction-sections -fexceptions -std=gnu++1y
+
+# Add ALL necessary include paths
+LOCAL_CFLAGS    += -I$(LOCAL_PATH)/../../first-party/fbjni/native/fb/include
+LOCAL_CFLAGS    += -I$(LOCAL_PATH)/../../native/cpp/include
+LOCAL_CFLAGS    += -I$(LOCAL_PATH)/../../first-party/include
+LOCAL_CFLAGS    += -I$(LOCAL_PATH)/../../third-party/openssl/include
+
 LOCAL_SRC_FILES := CipherHybrid.cpp DecryptHybrid.cpp EncryptHybrid.cpp JavaArrays.cpp JKeyChain.cpp KeyChainFromJava.cpp MacDecoderHybrid.cpp MacEncoderHybrid.cpp OnLoad.cpp PBKDF2Hybrid.cpp
 LOCAL_LDLIBS    := -llog
 LOCAL_LDFLAGS   += -Wl,--gc-sections -Wl,--exclude-libs,ALL
 
-LOCAL_STATIC_LIBRARIES = concealcpp
-LOCAL_STATIC_LIBRARIES += fb
+# Link against the libfb shared library
+LOCAL_SHARED_LIBRARIES := fb
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,cpp)
+# Import the libfb module
+$(call import-add-path,$(LOCAL_PATH)/../../first-party/fbjni/native)
 $(call import-module,fb)
